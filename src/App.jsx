4807c1feededa8699f94cfa7634233bfc9aeac7a
Antoine Pommier <140323098+AntoinePommier@users.noreply.github.com>
2026-03-17 22:16:35 +0900

    Added fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const pokemonList = ["eevee", "pikachu", "piplup", "sprigatito", "snorlax"];
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [cards, setCards] = useState([]);
+
+	useEffect(() => {
+		async function fetchPokemon() {
+			try {
+				const results = await Promise.all(
+					pokemonList.map(async (pokemon) => {
+						const res = await fetch(
+							`https://pokeapi.co/api/v2/pokemon/${pokemon}`,
+						);
+						if (!res.ok) {
+							throw new Error(`Error ${res.status}`);
+						}
+						const data = await res.json();
+						return {
+							name: data.name,
+							id: data.id,
+							sprite: data.sprites.front_default,
+						};
+					}),
+				);
+				console.log(results);
+				setCards(results);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		fetchPokemon();
+	}, []);
 
 	return (
 		<main>
 			<h1>Memory Card</h1>
-
-			<button type="button" onClick={() => setCount((c) => c + 1)}>
-				Count: {count}
-			</button>
+			{cards.map((card) => {
+				return <img src={card.sprite} alt="" key={card.id} />;
+			})}
 		</main>
 	);
 }
